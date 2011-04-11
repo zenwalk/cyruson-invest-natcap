f94eb575972f41e4b56a51930d553a8bd75dc8cd
richpsharp <richpsharp@localhost>
2011-04-11 12:07:14 -0700

    Parameterize functions, and modularize program.

--- python/agriculture.py
+++ python/agriculture.py
@@ -1,28 +1,69 @@
-import numpy
-import gdal
-from gdalconst import *
+import os, sys, time, numpy
+from osgeo import gdal
+from osgeo.gdalconst import *
 
-#open file
 FILENAME = '../sample_data/landuse_90'
-dataset = gdal.Open(FILENAME, GA_ReadOnly);
 CROPLANDID = 1
 
+def die(message):
+    print message
+    sys.exit(1)
+
+def countIds(band,id):
+    #Read dataset as 2D array, could kill memory
+    #array = band.ReadAsArray(0,0,band.XSize,band.YSize)
+    #print numpy.where(array == CROPLANDID)
+    #print numpy.where(array == CROPLANDID)[0].size
+    
+    #Loop through dataset by hand, very slow
+    #for x in range(0,band.XSize):
+    #    for y in range(0,band.YSize):
+    #        if array[y,x] == 1: 
+    #            croplandCount += 1
+    #print croplandCount
+
+    #Loop through line by line, good balance between memory footprint and runtime
+    croplandCount = 0
+    for y in range(band.YSize):
+        scanline = band.ReadAsArray(0,y,band.XSize, 1)
+        croplandCount += numpy.where(scanline == CROPLANDID)[0].size
+    return croplandCount
+
+
+startTime = time.time()
+
+#Register all drivers at once
+gdal.AllRegister()
+
+#open file
+dataset = gdal.Open(FILENAME, GA_ReadOnly);
+
 if dataset is None:
-    print 'barf'
+    die('Could not open ' + FILENAME)
 
 if (dataset.RasterCount != 1):
-    print 'RasterCount should be 1'+ dataset.RasterCount
-    
-band = dataset.GetRasterBand(1) 
+    die('RasterCount should be 1 '+ dataset.RasterCount)
 
 
-croplandCount = 0
+#define projection for indexing lat/lng
+projection = dataset.GetProjection()
 
-print 'X: ' + str(band.XSize) + ' Y: ' + str(band.YSize) 
 
-for i in range(band.YSize):
-    scanline = band.ReadAsArray(0,i,band.XSize, 1, band.XSize, 1)
-    croplandCount += numpy.where(scanline == CROPLANDID)[0].size
+#Get georeference info, coordinates are for top left corners of pixels
+geotransform = dataset.GetGeoTransform()
+originX = geotransform[0]
+originY = geotransform[3]
+pixelWidth = geotransform[1]
+pixelHeight = geotransform[5]
 
-#calculate number of nan cells?        
+#Get first band, should be only one since it's LULC    
+band = dataset.GetRasterBand(1)
+croplandCount = countIds(band,CROPLANDID)
+
+#Results
+print 'Projection: ' + projection
+print 'upper left: (' + str(originX) + ', ' + str(originY) + ')'
+print 'pixel dimensions (' + str(pixelWidth) + ', ' + str(pixelHeight) + ')'
+print 'X: ' + str(band.XSize) + ' Y: ' + str(band.YSize)
 print 'Total cropland: ' + str(croplandCount) + ' should be 9940'
+print 'Time elapsed: ' + str(time.time()-startTime) + ' seconds'
