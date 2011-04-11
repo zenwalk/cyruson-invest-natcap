37dd9f82c99598f7f42d710c8b56b34eaebba9a0
richpsharp <richpsharp@localhost>
2011-04-11 13:37:25 -0700

    Redefined function name so it's meaningful.

--- python/agriculture.py
+++ python/agriculture.py
@@ -3,18 +3,17 @@ from osgeo import gdal
 from osgeo import osr
 from osgeo.gdalconst import *
 
-FILENAME = '../sample_data/landuse_90'
+FILENAME = '../sample_data/lulc_samp_cur'
 CROPLANDID = 1
 
 def die(message):
     print message
     sys.exit(1)
 
-def countIds(band,id):
-    #Read dataset as 2D array, could kill memory
-    #array = band.ReadAsArray(0,0,band.XSize,band.YSize)
-    #print numpy.where(array == CROPLANDID)
-    #print numpy.where(array == CROPLANDID)[0].size
+def getCoordsWithId(band,id):
+    #Read dataset as 2D array, could kill memory -- should read as block
+    array = band.ReadAsArray(0,0,band.XSize,band.YSize)
+    return numpy.where(array == CROPLANDID)
     
     #Loop through dataset by hand, very slow
     #for x in range(0,band.XSize):
@@ -24,11 +23,11 @@ def countIds(band,id):
     #print croplandCount
 
     #Loop through line by line, good balance between memory footprint and runtime
-    croplandCount = 0
-    for y in range(band.YSize):
-        scanline = band.ReadAsArray(0,y,band.XSize, 1)
-        croplandCount += numpy.where(scanline == CROPLANDID)[0].size
-    return croplandCount
+    #croplandCount = 0
+    #for y in range(band.YSize):
+    #    scanline = band.ReadAsArray(0,y,band.XSize, 1)
+    #    croplandCount += numpy.where(scanline == CROPLANDID)[0].size
+    #return croplandCount
 
 #for timing runs
 startTime = time.time()
@@ -55,18 +54,24 @@ pixelHeight = geotransform[5]
 #define projection for indexing lat/lng
 srs = osr.SpatialReference()
 srs.ImportFromWkt(dataset.GetProjection())
+srsLatLong = srs.CloneGeogCS() #gets the coordinate system
+ct = osr.CoordinateTransformation(srs,srsLatLong) #transform from current projection to that projection's coordinate system
 
-srsLatLong = srs.CloneGeogCS() #gets the default coordinate system
-ct = osr.CoordinateTransformation(srs,srsLatLong) #transform from current projection to defined coordinate system
-print ct.TransformPoint(originX,originY)
 
 #Get first band, should be only one since it's LULC    
 band = dataset.GetRasterBand(1)
-croplandCount = countIds(band,CROPLANDID)
+
+#find the projected coordinates of all LULC with CROPLANDID
+idCoords = getCoordsWithId(band,CROPLANDID)
+
+#project to lat/lng
+croplandLatLng = ct.TransformPoints(zip(idCoords[0],idCoords[1]))
+
+
 
 #Results
 print 'upper left: (' + str(originX) + ', ' + str(originY) + ')'
 print 'pixel dimensions (' + str(pixelWidth) + ', ' + str(pixelHeight) + ')'
 print 'X: ' + str(band.XSize) + ' Y: ' + str(band.YSize)
-print 'Total cropland: ' + str(croplandCount) + ' should be 9940'
+print 'Total cropland: ' + str(len(croplandLatLng)) + ' should be 21992'
 print 'Time elapsed: ' + str(time.time()-startTime) + ' seconds'
