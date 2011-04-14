cb2e1f2182d925300f562b82a36fd86f7e83d8e5
richpsharp <richpsharp@localhost>
2011-04-14 12:34:51 -0700

    Modify unpickler so that it only unpickles crop data as needed.

--- utils/unpickle_m3_crop_data.py
+++ utils/unpickle_m3_crop_data.py
@@ -14,43 +14,55 @@ def die(message):
     print message
     sys.exit(1)
 
-#main script
-
-def unpickleBinaryMap(inFileMap):
+def unpickleCoordinates(inFileMap):
+    """Looks up coordinates and file positions for later access, this way
+    we don't load the entire dataset at once, only as needed"""
     nCoords = struct.unpack('!i',inFileMap.read(4))[0] # ncoords
     print nCoords
     globalMap = {}
     for i in range(nCoords):
-        if i % 100 == 0: #follow progress
+        if i % 10000 == 0: #follow progress
             percent = i/float(nCoords)
             print percent
             sys.stdout.flush()
-        coord = (struct.unpack_from('!h',inFileMap.read(2))[0],struct.unpack_from('!h',inFile.read(2))[0]) # x, y coord
-        #print coord
+        coord = (struct.unpack_from('!h',inFileMap.read(2))[0],struct.unpack_from('!h',inFileMap.read(2))[0]) # x, y coord
         nCrops = struct.unpack_from('!B',inFileMap.read(1))[0] #n crops
-        #print nCrops
-        cropMap = {}
-        for j in range(nCrops):
-            cropId = struct.unpack_from('!B',inFileMap.read(1))[0] #cropId
-            #print cropId 
-            cropArray = [None,None]
-            for k in range(2):
-                cropVal = struct.unpack_from('!f',inFileMap.read(4))[0]  # crop value (either area or yield) 
-                if cropVal != -1.0:
-                    cropArray[k] = cropVal
-            cropMap[cropId] = cropArray
-            #print cropArray
-        globalMap[coord] = cropMap
-    #print globalMap
+        #the -1 comes from the fact that we already read the nCrops, but we want to re-read it on the load
+        globalMap[coord] = inFileMap.tell()-1
+        #jump over the crop data 
+        inFileMap.seek((1+4+4)*nCrops,os.SEEK_CUR)
     return globalMap
 
-#This will map global coordinate tuples to a map of cropIds to yield/area arrays
+def getCropData(inFileMap,filePos):
+    """Parses out the crop data for the given file position"""
+    inFileMap.seek(filePos)
+    nCrops = struct.unpack_from('!B',inFileMap.read(1))[0] #n crops
+    
+    cropMap = {}
+    for j in range(nCrops):
+        cropId = struct.unpack_from('!B',inFileMap.read(1))[0] #cropId
+        print cropId 
+        cropArray = [None,None]
+        for k in range(2):
+            cropVal = struct.unpack_from('!f',inFileMap.read(4))[0]  # crop value (either area or yield) 
+            if cropVal != -1.0:
+                cropArray[k] = cropVal
+        cropMap[cropId] = cropArray
+    return cropMap
+    
+
+#This will map global coordinate tuples to a map of file positions that can 
+#later be read off disk to yield/area arrays
 globalMap = {}
 with open(FILENAME,'r+b') as inFile:
     inFileMap = mmap.mmap(inFile.fileno(),0)
-    globalMap = unpickleBinaryMap(inFileMap)
-    
+    globalMap = unpickleCoordinates(inFileMap)
+
+#globalMap is loaded, for fun look up some crop data    
 for coord in globalMap:
-    for crop in  globalMap[coord]:
-        print str(coord) + ', ' + str(crop) + ',' + str(globalMap[coord][crop])
-sys.stdout.flush()
+    print coord, globalMap[coord], getCropData(inFileMap, globalMap[coord])
+    keys = getCropData(inFileMap, globalMap[coord]).keys()
+    keys.sort()
+    print keys
+    sys.stdout.flush()
+    sys.exit(0)
