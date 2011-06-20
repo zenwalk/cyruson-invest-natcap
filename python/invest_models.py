c37c45d4c4061969d03a24fb2fd3802032e8a61b
richpsharp <richpsharp@localhost>
2011-06-20 12:13:32 -0700

    Building a test with all water cells.

--- python/invest_models.py
+++ python/invest_models.py
@@ -1,12 +1,16 @@
-import scipy
-def water_quality(grid, E, U, K, s0, h):
+from scipy.sparse import *
+from scipy import *
+from scipy.sparse.linalg import spsolve
+
+def water_quality(grid, E, Ux, Uy, K, s0, h):
     """2D Water quality model to track a pollutant in the ocean
     
     Keyword arguments:
     grid -- 2D grid of booleans indicating land/water.  True is water, False
             is land.
     E -- 2D grid of dispersion coefficients, must be same dimensions as 'grid'
-    U -- 2D grid of velocity vectors, must be same dimensions as 'grid'
+    Ux -- 2D grid of x component velocity vectors, must be same dimensions as 'grid'
+    Uy -- 2D grid of y component velocity vectors, must be same dimensions as 'grid'
     K -- 2D grid of decay coefficients, must be same dimensions as 'grid'
     s0 -- 2D array of pollutant density, must be same dimensions as 'grid'
     h -- scalar describing grid cell size
@@ -20,3 +24,32 @@ def water_quality(grid, E, U, K, s0, h):
 
 if __name__ == "__main__":
     pass #put unit tests here
+
+    #water quality test with all water
+    n = 10
+
+    #define land
+    row = array([x / n for x in range(n * n)])
+    col = array([x % n for x in range(n * n)])
+    data = array([True] * n * n)
+    grid = csr_matrix((data, (row, col)), shape=(n, n))
+
+    #cell size
+    h = 0.01
+
+    #define constants
+    E = csr_matrix((map(lambda x: 1.0, data), (row, col)), shape=(n, n))
+    Ux = csr_matrix((map(lambda x: 1.0, data), (row, col)), shape=(n, n))
+    Uy = csr_matrix((map(lambda x: 1.0, data), (row, col)), shape=(n, n))
+    K = csr_matrix((map(lambda x: 1.0, data), (row, col)), shape=(n, n))
+
+    #define source
+    row = array([n / 2])
+    col = array([n / 2])
+    data = array([1])
+    s0 = csr_matrix((data, (row, col)), shape=(n, n))
+
+
+
+    water_quality(grid, E, Ux, Uy, K, s0, h)
+
