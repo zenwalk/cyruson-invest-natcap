346a8c06439c0d69139c5ba51683b23b5456dce0
richpsharp <richpsharp@localhost>
2011-06-20 15:22:15 -0700

    Nicer test case that saves a .png of the result.

--- python/water_quality.py
+++ python/water_quality.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
 
     #water quality test with all water
     #allow an n*m rectangular grid
-    n, m = 3, 3
+    n, m = 100, 100
 
     #define land
     grid = [True] * n * m
@@ -16,15 +16,15 @@ if __name__ == "__main__":
     h = 0.01
 
     #define constants
-    E = map(lambda x: 0.1, grid)
-    Ux = map(lambda x: 0.0, grid)
-    Uy = map(lambda x: 0.0, grid)
-    K = map(lambda x: 0.0, grid)
+    E = map(lambda x: 1, grid)
+    Ux = map(lambda x:-11.0, grid)
+    Uy = map(lambda x: 11.0, grid)
+    K = map(lambda x: 1.5, grid)
 
     #define a source right in the middle
     row = n / 2
     col = m / 2
-    s0 = {row * m + col: 1}
+    s0 = {row * m + col: 100}
 
     X, Y = np.meshgrid(np.arange(0, m), np.arange(0, n))
 
@@ -43,9 +43,10 @@ if __name__ == "__main__":
     print result
     print Z
     print s0
-    plt.figure()
+    fig = plt.figure()
     plt.pcolor(X, Y, Z)
     plt.colorbar()
+    #fig.savefig('water.png', format='png')
     plt.show()
 
     print type(result)
