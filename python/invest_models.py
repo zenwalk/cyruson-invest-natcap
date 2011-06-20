c6a679062bb9f157f357762c39adcb2bc27b3291
richpsharp <richpsharp@localhost>
2011-06-20 13:04:01 -0700

    Water quality function complete but untested.

--- python/invest_models.py
+++ python/invest_models.py
@@ -8,19 +8,26 @@ def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
     Keyword arguments:
     n,m -- the number of rows,coluumns in the 2D grid.  Used to determine 
         indices into list parameters 'grid', 'E', 'Ux', 'Uy', and 'K' via
-        index (i,j) is position i*m+j in a list
+        sourceIndex (i,j) is position i*m+j in a list
     grid -- 1D list n*m elements long of booleans indicating land/water.  True
             is water, False is land.  
     E -- 1D list n*m elements long of dispersion coefficients
     Ux -- 1D list n*m elements long of x component velocity vectors
     Uy -- 1D list n*m elements long y component velocity vectors
     K -- 1D list n*m elements long of decay coefficients
-    s0 -- map of index to pollutant density
+    s0 -- map of sourceIndex to pollutant density
     h -- scalar describing grid cell size
     
     returns a 2D grid of pollutant densities in the same dimension as  'grid'
     
     """
+
+    #used to abstract the 2D to 1D index calculation below
+    def calc_index(i, j):
+        if i >= 0 and i < n and j >= 0 and j < m:
+            return i * m + j
+        else:
+            return - 1
 
     #set up variables to hold the sparse system of equations
     col = []
@@ -31,22 +38,46 @@ def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
     #this map is used to quickly test if a neighboring element is a water cell
 
     #iterate over the non-zero elements in grid to build the linear system
-    for row in range(n):
-        col, val = grid[row].nonzero()
-        for i in range(len(col)):
-            print col[i], val[i]
+    for i in range(n):
+        for j in range(m):
+            #diagonal element i,j
+            sourceIndex = calc_index(i, j)
+            row.append(sourceIndex)
+            col.append(sourceIndex)
+            data.append(-(8.0 * E[sourceIndex] + 2 * h * h * K[sourceIndex]))
+            b.append(0) #initialize source vector
 
-    matrix = csr_matrix((data, (row, col)), shape=(n, m))
+            #formulate the nondiagonal elements as a single array 
+            elements = [(calc_index(i + 1, j), 2 * E[sourceIndex] -
+                         Ux[sourceIndex] * h),
+             (calc_index(i - 1, j), 2 * E[sourceIndex] + Ux[sourceIndex] * h),
+             (calc_index(i, j + 1), 2 * E[sourceIndex] - Uy[sourceIndex] * h),
+             (calc_index(i, j - 1), 2 * E[sourceIndex] + Uy[sourceIndex] * h)]
 
-    return True
+            #process nondiagonal elements.  might be a source, might not...
+            for tmpIndex, term in elements:
+                if tmpIndex >= 0:
+                    if tmpIndex not in s0:
+                        row.append(sourceIndex)
+                        col.append(tmpIndex)
+                        data.append(term)
+                    else:
+                        b[sourceIndex] += s0[tmpIndex] * (-term)
 
+    #stamp into numpy formulation to be solved
+    row = array(row)
+    col = array(col)
+    data = array(data)
+    b = array(b)
+    matrix = csr_matrix((data, (row, col)), shape=(n * m, n * m))
+    return spsolve(matrix, b)
 
 if __name__ == "__main__":
     pass #put unit tests here
 
     #water quality test with all water
     #allow an n*m rectangular grid
-    n, m = 10, 10
+    n, m = 3, 3
 
     #define land
     grid = [True] * n * m
@@ -61,9 +92,9 @@ if __name__ == "__main__":
     K = map(lambda x: 1.0, grid)
 
     #define a source right in the middle
-    row = array([n / 2])
-    col = array([m / 2])
-    so = {(row, col): 1}
+    row = n / 2
+    col = m / 2
+    s0 = {row * m + col: 1}
 
-    water_quality(grid, E, Ux, Uy, K, s0, h)
+    result = water_quality(n, m, grid, E, Ux, Uy, K, s0, h)
 
