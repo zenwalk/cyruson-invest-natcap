5a33688b47b42ada4ca10b63c387fcde7940ecad
richpsharp <richpsharp@localhost>
2011-06-22 13:26:11 -0700

    Rearrangement of boundary conditions for smaller and faster code footprint (40% speedup).

--- python/invest_models.py
+++ python/invest_models.py
@@ -3,7 +3,7 @@ from scipy.sparse.linalg import *
 from scipy import *
 from scipy.sparse.linalg import spsolve
 import numpy as np
-
+import time
 
 def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
     """2D Water quality model to track a pollutant in the ocean
@@ -25,6 +25,9 @@ def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
     
     """
 
+    print 'initialize ...',
+    t0 = time.clock()
+
     #used to abstract the 2D to 1D index calculation below
     def calc_index(i, j):
         if i >= 0 and i < n and j >= 0 and j < m:
@@ -33,20 +36,40 @@ def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
             return - 1
 
     #set up variables to hold the sparse system of equations
-    col = []
-    row = []
-    data = []
-    b = []
-
+    #upper bound  n*m*5 elements minus 4*the number of land cells
+    maxElements = n * m * 5 - grid.count(True) * 4
+    col = np.empty(maxElements)
+    row = np.empty(maxElements)
+    data = np.empty(maxElements)
+    b = np.zeros(n * m)
+    currentIndex = 0
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    t0 = time.clock()
     #this map is used to quickly test if a neighboring element is a water cell
 
-    #iterate over the non-zero elements in grid to build the linear system
-    print 'building system'
+    #iterate over the non-zero elnp.array([])ts in grid to build the linear system
+    print 'building system ...',
+    t0 = time.clock()
+    case1 = 0
+    case2 = 0
+    case3 = 0
+    case4 = 0
     for i in range(n):
         for j in range(m):
             #diagonal element i,j
             rowIndex = calc_index(i, j)
-            b.append(0) #initialize source vector
+
+            #check for boundary condition. if i,j on source or land
+            if not grid[rowIndex] or rowIndex in s0:
+                #not water, just set to 0 and quit
+                row[currentIndex] = col[currentIndex] = rowIndex
+                data[currentIndex] = 1
+                currentIndex += 1
+                if rowIndex in s0:
+                    b[rowIndex] = s0[rowIndex]
+                    case3 += 1
+                case1 += 1
+                continue
 
             #formulate elements as a single array 
             elements = [
@@ -55,58 +78,37 @@ def water_quality(n, m, grid, E, Ux, Uy, K, s0, h):
              (calc_index(i - 1, j), 2 * E[rowIndex] + Ux[rowIndex] * h),
              (calc_index(i, j + 1), 2 * E[rowIndex] - Uy[rowIndex] * h),
              (calc_index(i, j - 1), 2 * E[rowIndex] + Uy[rowIndex] * h)]
-            elementNum = 0
             #process elements.  might be a source, might not...
+            startIndex = currentIndex
+
             for colIndex, term in elements:
-                if colIndex >= 0:
-                    if grid[colIndex]: #check if water
-                        if colIndex not in s0: #check if source
-                            row.append(rowIndex)
-                            col.append(colIndex)
-                            data.append(term)
-                            elementNum += 1
-                        else:
-                            #test to see if current row is a source
-                            if rowIndex == colIndex:
-                                #we encounter the diagonal first because of 
-                                #the way we defined elements.  Set that row
-                                #to a 1 on the diagonal and put the source
-                                #on the forcing function side
-                                row.append(rowIndex)
-                                col.append(colIndex)
-                                data.append(1)
-                                elementNum += 1
-                                b[rowIndex] = s0[colIndex]
-                                #no need to process the rest of the elements
-                                break
-                            else :
-                                #we know what that variable is, so just push
-                                #it's value to the b vector
-                                b[rowIndex] += s0[colIndex] * (-term)
+                if colIndex >= 0: #make sure we're in the grid
+                    if not grid[colIndex]:
+                        #handle the land boundary case s_ij' = s_ij
+                        data[startIndex] += term
+                        case2 += 1
                     else:
-                        #we're on a land point, define s as 0
-                        if rowIndex == colIndex: #check if we're on a land point
-                            row.append(rowIndex)
-                            col.append(colIndex)
-                            data.append(1)
-                            elementNum += 1
-                            #b is implicitly zero, no need to process the rest
-                            #of the elements
-                            break
-                        else:
-                            #handle the land boundary case s_ij' = s_ij
-                            data[len(data) - elementNum] += term
+                        row[currentIndex] = rowIndex
+                        col[currentIndex] = colIndex
+                        data[currentIndex] = term
+                        currentIndex += 1
+                        case4 += 1
 
-
-
-    print 'building sparse matrix'
+    print case1, case2, case3, case4
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    #truncate the unused columns off of the sparse matrix arrays
+    print 'building sparse matrix ...',
+    t0 = time.clock()
+    col = np.delete(col, np.s_[currentIndex::])
+    row = np.delete(row, np.s_[currentIndex::])
+    data = np.delete(data, np.s_[currentIndex::])
     #stamp into numpy formulation to be solved
-    row = array(row)
-    col = array(col)
-    data = array(data)
-    b = array(b)
     matrix = csc_matrix((data, (row, col)), shape=(n * m, n * m))
 
-    print 'solving'
-    return spsolve(matrix, b)
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    print 'solving ...',
+    t0 = time.clock()
+    result = spsolve(matrix, b)
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    return result
 
