9af0cccae4043d95b337e1e430a01e3de98acb22
Richard Sharp <richpsharp@gmail.com>
2011-06-27 02:15:22 -0700

    WOrking on time domain solution.  STill buggy.

--- python/invest_models.py
+++ python/invest_models.py
@@ -36,13 +36,168 @@ def water_quality_1d(n, m, E, Ux, Uy, K, s0, h):
                 continue
             alpha = Ux[index] / (2 * E[index]) * (1 - math.sqrt(1 + 4 * K[index] * E[index] / (Ux[index] * Ux[index])))
             for p in s0:
-                i0, j0 = p % m, p / m
+                j0, i0 = p % m, p / m
                 val = s0[p]
                 d = math.sqrt(math.pow(h * (i - i0), 2) + math.pow(h * (j - j0), 2))
                 x0[index] += val * math.exp(d * alpha)
     return x0
 
     print '(' + str(time.clock() - t0) + 's elapsed)'
+
+def water_quality_time(n, m, tsteps, inWater, E, Ux, Uy, K, s0, h, dt, directSolve=False):
+    """2D Water quality model to track a pollutant in the ocean
+    
+    Keyword arguments:
+    n,m -- the number of rows, columns in the 2D grid.  Used to determine 
+        indices into list parameters 'water', 'E', 'Ux', 'Uy', and 'K' i*m+j in
+        a list
+    tsteps -- number of timesteps to simulate
+    water -- 1D list n*m elements long of booleans indicating land/water.  True
+            is water, False is land.  
+    E -- 1D list n*m elements long of dispersion coefficients
+    Ux -- 1D list n*m elements long of x component velocity vectors
+    Uy -- 1D list n*m elements long y component velocity vectors
+    K -- 1D list n*m elements long of decay coefficients
+    s0 -- map of sourceIndex to pollutant density
+    h -- scalar describing grid cell size
+    dt -- the delta time step
+    directSolve -- if True uses a direct solver that may be faster, but use
+        more memory.  May crash in cases where memory is fragmented or low
+        Default False.
+    
+    returns an array of 2D grid of pollutant densities in the same dimension as  'grid' with tsteps values for each timestep
+    
+    """
+
+    print 'initialize ...',
+    t0 = time.clock()
+
+    #used to abstract the 2D to 1D index calculation below
+    def calc_index(i, j):
+        if i >= 0 and i < n and j >= 0 and j < m:
+            return i * m + j
+        else:
+            return - 1
+
+    #set up variables to hold the sparse system of equations
+    #upper bound  n*m*5 elements
+    b = np.zeros(n * m)
+    #holds the columns for diagonal sparse matrix creation later
+    A = np.zeros((5, n * m))
+
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    t0 = time.clock()
+
+    #iterate over the non-zero elments in grid to build the linear system
+    print 'building system A...',
+    t0 = time.clock()
+    for i in range(n):
+        for j in range(m):
+            #diagonal element i,j always in bounds, calculate directly
+            rowIndex = calc_index(i, j)
+
+            #if land then s = 0 and quit
+            if not inWater[rowIndex]:
+                A[2, rowIndex] = 1
+                continue
+
+            #formulate elements as a single array
+            termA = 2 * E[rowIndex]
+            Uxtmp = Ux[rowIndex] * h
+            Uytmp = Uy[rowIndex] * h
+
+            elements = [
+             (2, 0, rowIndex, -(2*dt*(2*E[rowIndex]+2*E[rowIndex]+K[rowIndex]*h*h)+4*h*h)),
+             (4, m, calc_index(i+1, j), dt*(termA + Uxtmp)),
+             (0, -m, calc_index(i - 1, j), dt*(termA - Uxtmp)),
+             (3, 1, calc_index(i, j + 1), dt*(termA + Uytmp)),
+             (1, -1, calc_index(i, j - 1), dt*(termA - Uytmp))]
+
+            for k, offset, colIndex, term in elements:
+                if colIndex >= 0: #make sure we're in the grid
+                    if inWater[colIndex]: #if water
+                        A[k, rowIndex + offset] += term
+                    else:
+                        #handle the land boundary case s_ij' = s_ij
+                        A[2, rowIndex] += term
+
+    #define sources by erasing the rows in the matrix that have already been set
+    for rowIndex in s0:
+        #the magic numbers are the diagonals and their offsets due to gridsize
+        for i, offset in [(4, m), (0, -m), (3, 1), (1, -1)]:
+            #zero out that row
+            A[i, rowIndex + offset] = 0
+        A[2, rowIndex] = 1
+        b[rowIndex] = s0[rowIndex]
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+
+    print 'building sparse matrix ...',
+    t0 = time.clock()
+    matrix = spdiags(A, [-m, -1, 0, 1, m], n * m, n * m, "csc")
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+
+    print 'generating preconditioner via sparse ilu ',
+        #normally factor will use m*(n*m) extra space, we restrict to 
+        #\sqrt{m}*(n*m) extra space
+    P = scipy.sparse.linalg.spilu(matrix, fill_factor=int(math.sqrt(m)))
+    print '(' + str(time.clock() - t0) + 's elapsed)'
+    t0 = time.clock()
+        #create linear operator for precondioner
+    M_x = lambda x: P.solve(x)
+    M = scipy.sparse.linalg.LinearOperator((n * m, n * m), M_x)
+    
+    result = None
+    #initial solution vector
+    x=b
+    for step in range(tsteps):
+        print "gmres iteration starting step", step, "of ", tsteps,
+        x = scipy.sparse.linalg.lgmres(matrix, b, x0=x,tol=1e-5, M=M)[0]
+        print x[20100]
+        print b[20100]
+        print 'sum x: ', np.sum(x)
+        if result == None:
+            result = x
+        else:
+            result=np.append(result,x)
+        print len(result)
+        #update b vector for next timestep
+        for i in range(n):
+            for j in range(m):
+                rowIndex = calc_index(i, j)
+                b[rowIndex] = 4*h*h*x[rowIndex]
+
+                #formulate elements as a single array
+                termA = 2 * E[rowIndex]
+                Uxtmp = Ux[rowIndex] * h
+                Uytmp = Uy[rowIndex] * h
+
+                elements = [
+                    (2, 0, rowIndex, -(2*dt*(2*E[rowIndex]+2*E[rowIndex]+K[rowIndex]*h*h)+4*h*h)),
+                    (4, m, calc_index(i+1, j), dt*(termA + Uxtmp)),
+                    (0, -m, calc_index(i - 1, j), dt*(termA - Uxtmp)),
+                    (3, 1, calc_index(i, j + 1), dt*(termA + Uytmp)),
+                    (1, -1, calc_index(i, j - 1), dt*(termA - Uytmp))]
+
+                for k, offset, colIndex, term in elements:
+                    if colIndex >= 0: #make sure we're in the grid
+                        if inWater[colIndex]: #if water
+                            b[rowIndex] += x[colIndex]*term
+        b[rowIndex] = -b[rowIndex]
+
+        #define sources by erasing the rows in the matrix that have already been set
+        for rowIndex in s0:
+            print rowIndex
+            b[rowIndex] = s0[rowIndex]
+                    
+        print '(' + str(time.clock() - t0) + 's elapsed)'
+        t0 = time.clock()
+
+    print 'done returning', len(result)
+    return result.reshape(tsteps,n*m)
+
+
+
+
 
 def water_quality(n, m, inWater, E, Ux, Uy, K, s0, h, directSolve=False):
     """2D Water quality model to track a pollutant in the ocean
--- python/water_quality.py
+++ python/water_quality.py
@@ -8,11 +8,11 @@ def plotResult(land, result, n, m, dimx, dimy, h):
 
 
 
-    print 'prepare result plot'
+#    print 'prepare result plot'
     result = np.array(result).reshape((n, m))
-    print 'plot result'
+#    print 'plot result'
     plt.pcolormesh(X, Y, result, cmap=plt.cm.gist_earth)
-    plt.colorbar()
+#    plt.colorbar()
 
     if False in land:
         print 'generate land mask'
@@ -20,6 +20,90 @@ def plotResult(land, result, n, m, dimx, dimy, h):
         Zmask = np.ma.array(Z, mask=land)
         print 'plot land grid'
         plt.pcolormesh(X, Y, Zmask, cmap=plt.cm.BrBG)
+
+def test4():
+    #water quality test with all water
+
+    #cell size
+    h = 0.5 #0.01km
+
+    #square size
+    dimx = 100
+    dimy = 100
+    dt = 0.01
+    tsteps = 9
+
+    n, m = int(dimy / h), int(dimx / h)
+
+    #define land
+    grid = [True] * n * m
+
+    print "elements: ", n * m
+
+    #define constants
+    E = map(lambda x: 4, grid)
+    Ux = map(lambda x:2.0, grid)
+    Uy = map(lambda x:2.0, grid)
+    K = map(lambda x: 0.1, grid)
+
+    #define a source right in the middle
+    row = int(n / 2)
+    col = int(m / 2)
+    s0 = {row * m + col: 1}
+
+    result2d_td = water_quality_time(n, m, tsteps, grid, E, Ux, Uy, K, s0, h, dt)
+
+    fig = plt.figure()
+    for step in range(tsteps):
+        f = fig.add_subplot("1"+str(tsteps)+str(step+1), aspect='equal')
+        plotResult(grid, result2d_td[step], n, m, dimx, dimy, h)
+
+def test3():
+    #time domain test
+
+    #define land from file
+    print 'load file'
+    f = open('cell.inp', 'r')
+    n = int(f.readline())
+    m = int(f.readline())
+    h = float(f.readline())
+    gridText = f.readline()
+    dt = 0.1
+    tsteps = 2
+    print n, m, h
+
+    grid = []
+    for c in gridText:
+        if c == '1':
+            grid.append(True)
+        else:
+            grid.append(False)
+
+    print len(grid)
+
+    dimx, dimy = m * h, n * h
+
+    #define constants
+    E = map(lambda x: 4.0, grid)
+    Ux = map(lambda x:0.0707, grid)
+    Uy = map(lambda x:0.0, grid)
+    K = map(lambda x: 0.04, grid)
+
+    #define a source right in the middle
+    row = 342
+    col = 291
+    s0 = {row * m + col: 1}
+
+    print row, col
+
+    result2d_td = water_quality_time(n, m, tsteps, grid, E, Ux, Uy, K, s0, h, dt)
+
+    fig = plt.figure()
+    f = fig.add_subplot(121, aspect='equal')
+    plotResult(grid, result2d_td[0], n, m, dimx, dimy, h)
+    f = fig.add_subplot(122, aspect='equal')
+    plotResult(grid, result2d_td[1], n, m, dimx, dimy, h)
+
 
 def test2():
     #water quality test with all water
@@ -51,8 +135,8 @@ def test2():
     K = map(lambda x: 0.04, grid)
 
     #define a source right in the middle
-    row = 291
-    col = 342
+    row = 342
+    col = 291
     s0 = {row * m + col: 1}
 
     print row, col
@@ -106,6 +190,5 @@ def test1():
 
 
 if __name__ == "__main__":
-    test1()
-    test2()
+    test4()
     plt.show()
