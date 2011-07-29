022cc53867e9dcaad724f714c510111d91f78247
richpsharp <richpsharp@localhost>
2011-07-29 15:51:45 -0700

    buildit.sh now takes a command line argument specifying the release number.

--- installer/buildit.sh
+++ installer/buildit.sh
@@ -1,1 +1,10 @@
-wine ~/.wine/drive_c/Program\ Files/NSIS/makensis.exe ./invest_install_build.nsi
+#!/bin/bash
+
+V=$1
+
+if [ $# -eq 0 ]
+then
+  V=2.2
+fi
+
+wine ~/.wine/drive_c/Program\ Files/NSIS/makensis.exe /DVERSION=$V ./invest_install_build.nsi
