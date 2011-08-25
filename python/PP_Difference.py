2e1be831ba6f3d1ed228c6332e829b63a2206c64
gverutes <gverutes@ncp-verutes.stanford.edu>
2011-08-25 17:50:26 -0400

    Updated Post-processing scripts based on Beta testing

--- python/PP_Difference.py
+++ python/PP_Difference.py
@@ -1,6 +1,6 @@
 # Marine InVEST: Difference Tool
 # Author: Gregg Verutes
-# 08/23/11
+# 08/25/11
 
 # import modules
 import sys, string, os, datetime
@@ -30,12 +30,30 @@ try:
         raise Exception, msgArguments + gp.GetMessages(2)
 
     gp.workspace = outputRst[:-(1+len(outputRst.split("\\")[-1]))]
+
+    # intermediate variables
+    cur_isNull = gp.workspace+"\\cur_isnull"
+    fut_isNull = gp.workspace+"\\fut_isnull"
+    cur_con = gp.workspace+"\\cur_con"
+    fut_con = gp.workspace+"\\fut_con"
+
     gp.Extent = "MAXOF"
 
     try:
+        # convert no data to "0" without losing floating point accuracy
+        gp.IsNull_sa(currentRst, cur_isNull)
+        gp.Con_sa(cur_isNull, "0", cur_con, currentRst, "VALUE = 1")
+        gp.IsNull_sa(futureRst, fut_isNull)
+        gp.Con_sa(fut_isNull, "0", fut_con, futureRst, "VALUE = 1")
+        
         # subtract current from future
-        DiffExpr = futureRst+" - "+currentRst
-        gp.SingleOutputMapAlgebra_sa(DiffExpr, outputRst)
+        gp.Minus_sa(fut_con, cur_con, outputRst)
+
+        # delete intermediate data
+        gp.delete_management(cur_isNull)
+        gp.delete_management(fut_isNull)
+        gp.delete_management(cur_con)
+        gp.delete_management(fut_con)
     except:
         gp.AddError(msgEraseFunction)
         raise Exception
