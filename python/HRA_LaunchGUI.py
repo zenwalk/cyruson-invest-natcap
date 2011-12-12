ea373aad8fda4b0dac54496498123ada734f8806
gverutes <gverutes@ncp-verutes.stanford.edu>
2011-12-12 13:38:45 -0500

    Merge with default

--- python/HRA_LaunchGUI.py
+++ python/HRA_LaunchGUI.py
@@ -1,5 +1,5 @@
-import os
+import os, sys
 
 if __name__=="__main__":
-    os.system("start HRA_SurveyTool.py")
+    os.system("start "+sys.argv[0][:sys.argv[0].rfind("\\")]+"\\HRA_SurveyTool.py")
     
