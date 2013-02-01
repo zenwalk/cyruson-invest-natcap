494627ff208f849447fd97496d7329099ef02bb8
stacie wolny <stacie.wolny@gmail.com>
2013-02-01 11:24:38 -0800

    Now use MultiValue inputs to allow for arbitrary # of services

--- utils/Multi_service_rank.py
+++ utils/Multi_service_rank.py
@@ -41,175 +41,24 @@ try:
         gp.workspace = gp.GetParameterAsText(0)
         parameters.append("Workspace: " + gp.workspace)
 
-        # Check and create output folders
-        try:
-            thefolders=["Post_process", "Intermediate"]
-            for folder in thefolders:
-                if not gp.Exists(gp.workspace+folder):
-                    gp.CreateFolder_management(gp.workspace, folder)
+        # Services - list of service outputs from an InVEST model
+        services = gp.GetParameterAsText(1)
+        parameters.append("Services: " + services)
+        service_list = services.split(";")
 
-            postprocws = gp.workspace + os.sep + "Post_process" + os.sep
-            interws = gp.workspace + os.sep + "Intermediate" + os.sep
-        except:
-            gp.AddError("Error creating output folders: " + gp.GetMessages())
+        # Weights - list of floating point values,
+        #  weight to multiply all values in corresponding services by
+        weights = gp.GetParameterAsText(2)
+        parameters.append("Weights: " + weights)
+        weight_list = weights.split(";")
+
+        # Make sure that number of weights matches number of services
+        if len(service_list) <> len(weight_list):
+            gp.AddError("\nError: The number of services entered must match the number of weights.")
             raise Exception
 
-        # Service 1 - service output from an InVEST model
-        service1 = gp.GetParameterAsText(1)
-        parameters.append("Service 1: " + service1)
-
-        # Weight 1 - floating point value, weight to multiply all values in Service 1 by
-        weight1 = gp.GetParameterAsText(2)
-        parameters.append("Weight 1: " + weight1)
-
-        # At least one service/weight is required, so no need to check for existence
-        # Normalize service
-        service1_norm = interws + "serv1_norm"
-        service_max = gp.GetRasterProperties_management(service1, "MAXIMUM")
-        gp.Divide_sa(service1, service_max, service1_norm)
-        # Create string of service/weight pairs required for Arc's Weighted Sum function             
-        overlay_input = service1_norm + " VALUE " + str(weight1)
-
-        # Service 2 - service output from an InVEST model
-        service2 = gp.GetParameterAsText(3)
-        parameters.append("Service 2: " + service2)
-
-        # Weight 2 - floating point value, weight to multiply all values in Service 2 by
-        weight2 = gp.GetParameterAsText(4)
-        parameters.append("Weight 2: " + weight2)
-
-        # If service 2 is entered, a corresponding weight must be entered too
-        if ((service2 != "") and (service2 != string.whitespace) and (service2 != "#")) \
-           and ((weight2 == "") or (weight2 == string.whitespace) or (weight2 == "#")):
-            gp.AddError("\nError: If service 2 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service2 != "") and (service2 != string.whitespace) and (service2 != "#")) \
-           and ((weight2 != "") and (weight2 != string.whitespace) and (weight2 != "#")):
-            # Normalize service
-            service2_norm = interws + "serv2_norm"
-            service_max = gp.GetRasterProperties_management(service2, "MAXIMUM")
-            gp.Divide_sa(service2, service_max, service2_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service2_norm + " VALUE " + str(weight2)
-
-        # Service 3 - service output from an InVEST model
-        service3 = gp.GetParameterAsText(5)
-        parameters.append("Service 3: " + service3)
-
-        # Weight 3 - floating point value, weight to multiply all values in Service 3 by
-        weight3 = gp.GetParameterAsText(6)
-        parameters.append("Weight 3: " + weight3)
-
-        # If service 3 is entered, a corresponding weight must be entered too
-        if ((service3 != "") and (service3 != string.whitespace) and (service3 != "#")) \
-           and ((weight3 == "") or (weight3 == string.whitespace) or (weight3 == "#")):
-            gp.AddError("\nError: If service 3 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service3 != "") and (service3 != string.whitespace) and (service3 != "#")) \
-           and ((weight3 != "") and (weight3 != string.whitespace) and (weight3 != "#")):
-            # Normalize service
-            service3_norm = interws + "serv3_norm"
-            service_max = gp.GetRasterProperties_management(service3, "MAXIMUM")
-            gp.Divide_sa(service3, service_max, service3_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service3_norm + " VALUE " + str(weight3)
-
-        # Service 4 - service output from an InVEST model
-        service4 = gp.GetParameterAsText(7)
-        parameters.append("Service 4: " + service4)
-
-        # Weight 4 - floating point value, weight to multiply all values in Service 4 by
-        weight4 = gp.GetParameterAsText(8)
-        parameters.append("Weight 4: " + weight4)
-
-        # If service 4 is entered, a corresponding weight must be entered too
-        if ((service4 != "") and (service4 != string.whitespace) and (service4 != "#")) \
-           and ((weight4 == "") or (weight4 == string.whitespace) or (weight4 == "#")):
-            gp.AddError("\nError: If service 4 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service4 != "") and (service4 != string.whitespace) and (service4 != "#")) \
-           and ((weight4 != "") and (weight4 != string.whitespace) and (weight4 != "#")):
-            # Normalize service
-            service4_norm = interws + "serv4_norm"
-            service_max = gp.GetRasterProperties_management(service4, "MAXIMUM")
-            gp.Divide_sa(service4, service_max, service4_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service4_norm + " VALUE " + str(weight4)
-
-        # Service 5 - service output from an InVEST model
-        service5 = gp.GetParameterAsText(9)
-        parameters.append("Service 5: " + service5)
-
-        # Weight 5 - floating point value, weight to multiply all values in Service 5 by
-        weight5 = gp.GetParameterAsText(10)
-        parameters.append("Weight 5: " + weight5)
-
-        # If service 5 is entered, a corresponding weight must be entered too
-        if ((service5 != "") and (service5 != string.whitespace) and (service5 != "#")) \
-           and ((weight5 == "") or (weight5 == string.whitespace) or (weight5 == "#")):
-            gp.AddError("\nError: If service 5 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service5 != "") and (service5 != string.whitespace) and (service5 != "#")) \
-           and ((weight5 != "") and (weight5 != string.whitespace) and (weight5 != "#")):
-            # Normalize service
-            service5_norm = interws + "serv5_norm"
-            service_max = gp.GetRasterProperties_management(service5, "MAXIMUM")
-            gp.Divide_sa(service5, service_max, service5_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service5_norm + " VALUE " + str(weight5)
-
-        # Service 6 - service output from an InVEST model
-        service6 = gp.GetParameterAsText(11)
-        parameters.append("Service 6: " + service6)
-
-        # Weight 6 - floating point value, weight to multiply all values in Service 6 by
-        weight6 = gp.GetParameterAsText(12)
-        parameters.append("Weight 6: " + weight6)
-
-        # If service 6 is entered, a corresponding weight must be entered too
-        if ((service6 != "") and (service6 != string.whitespace) and (service6 != "#")) \
-           and ((weight6 == "") or (weight6 == string.whitespace) or (weight6 == "#")):
-            gp.AddError("\nError: If service 6 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service6 != "") and (service6 != string.whitespace) and (service6 != "#")) \
-           and ((weight6 != "") and (weight6 != string.whitespace) and (weight6 != "#")):
-            # Normalize service
-            service6_norm = interws + "serv6_norm"
-            service_max = gp.GetRasterProperties_management(service6, "MAXIMUM")
-            gp.Divide_sa(service6, service_max, service6_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service6_norm + " VALUE " + str(weight6)
-
-        # Service 7 - service output from an InVEST model
-        service7 = gp.GetParameterAsText(13)
-        parameters.append("Service 7: " + service7)
-
-        # Weight 7 - floating point value, weight to multiply all values in Service 7 by
-        weight7 = gp.GetParameterAsText(14)
-        parameters.append("Weight 7: " + weight7)
-
-        # If service 7 is entered, a corresponding weight must be entered too
-        if ((service7 != "") and (service7 != string.whitespace) and (service7 != "#")) \
-           and ((weight7 == "") or (weight7 == string.whitespace) or (weight7 == "#")):
-            gp.AddError("\nError: If service 7 is to be processed, a corresponding weight must be provided")
-            raise Exception
-        # If both service and weight are entered, process them
-        elif ((service7 != "") and (service7 != string.whitespace) and (service7 != "#")) \
-           and ((weight7 != "") and (weight7 != string.whitespace) and (weight7 != "#")):
-            # Normalize service
-            service7_norm = interws + "serv7_norm"
-            service_max = gp.GetRasterProperties_management(service7, "MAXIMUM")
-            gp.Divide_sa(service7, service_max, service7_norm)
-            # Create string of service/weight pairs required for Arc's Weighted Sum function             
-            overlay_input = overlay_input + "; " + service7_norm + " VALUE " + str(weight7)
-            
         # Optional percent value for slicing result (to see top X% of service provision)
-        percent = gp.GetParameterAsText(15)
+        percent = gp.GetParameterAsText(3)
         parameters.append("Grouping percent" + str(percent))
         if ((percent == "") or (percent == string.whitespace) or (percent == "#")):
             do_percent = False
@@ -217,11 +66,11 @@ try:
             do_percent = True
 
         # Group by X% of ranking value distribution?
-        group_by_dist = gp.GetParameterAsText(16)
-        parameters.append("Group by value: " + str(group_by_dist))
+        group_by_dist = gp.GetParameterAsText(4)
+        parameters.append("Group by value distribution: " + str(group_by_dist))
 
         # Group by X% of area?
-        group_by_area = gp.GetParameterAsText(17)
+        group_by_area = gp.GetParameterAsText(5)
         parameters.append("Group by area: " + group_by_area)
 
         if do_percent and group_by_dist == 'false' and group_by_area == 'false':
@@ -233,7 +82,7 @@ try:
             raise Exception
 
         # Suffix to append to output filenames, as <filename>_<suffix>
-        Suffix = gp.GetParameterAsText(18)
+        Suffix = gp.GetParameterAsText(6)
         parameters.append("Suffix: " + Suffix)
 
         if (Suffix == "") or (Suffix == string.whitespace) or (Suffix == "#"):
@@ -245,11 +94,25 @@ try:
         gp.AddError("\nError in input arguments: " + gp.GetMessages(2))
         raise Exception
 
+
+    # Check and create output folders
+    try:
+        thefolders=["Post_process", "Intermediate"]
+        for folder in thefolders:
+            if not gp.Exists(gp.workspace+folder):
+                gp.CreateFolder_management(gp.workspace, folder)
+
+        postprocws = gp.workspace + os.sep + "Post_process" + os.sep
+        interws = gp.workspace + os.sep + "Intermediate" + os.sep
+    except:
+        gp.AddError("Error creating output folders: " + gp.GetMessages())
+        raise Exception
+
+
     # Script-created files
     try:
 
         # Intermediate files
-        service_norm = interws + "serv_norm"
         service_nw = interws + "serv_nw"
         service_sum_tmp = interws + "serv_sum_tmp"
         weighted_service_slice = interws + "serv_slice"
@@ -277,6 +140,28 @@ try:
     
     # Process inputs
     try:
+
+        # Process all services entered to make the combined ranking raster
+        
+        service_num = 0
+        
+        for service in service_list:
+            # Normalize the service by its maximum value
+            service_norm = interws + "serv" + str(service_num) + "_norm"
+            service_max = gp.GetRasterProperties_management(service, "MAXIMUM")
+            gp.Divide_sa(service, service_max, service_norm)
+
+            service_weight = float(weight_list[service_num])
+            
+            # Create string of service/weight pairs required for Arc's Weighted Sum function             
+            overlay_input = service_norm + " VALUE " + str(service_weight) + ";"
+
+            service_num += 1
+
+        # Remove trailing semicolon from string of service/weight pairs
+        overlay_input = overlay_input[:-1]
+        
+        # Use Weighted Sum to combine normalized services and weights
         gp.WeightedSum_sa(overlay_input, weighted_service_sum)
 
         gp.AddMessage("\nCreated combined services ranking output: \n\t" + weighted_service_sum)
@@ -292,14 +177,12 @@ try:
                 group_area_zstat_table = interws + "group_area_zstat.dbf"
                 group_dist_poly_dissolve = interws + "group_dist_poly_dissolve.shp"
                 weighted_service_group_dist_poly = interws + "combined_service_group_by_dist.shp"
-                weighted_service_group_area_poly = interws + "combined_service_group_by_value.shp"
+                weighted_service_group_area_poly = interws + "combined_service_group_by_area.shp"
 
                 weighted_service_group_dist_ras = postprocws + "combined_service_group_by_distribution" + Suffix + ".tif"
                 weighted_service_group_dist_poly_final = postprocws + "combined_service_group_by_distribution" + Suffix + ".shp"
-                weighted_service_group_area_ras = postprocws + "combined_service_group_by_value" + Suffix + ".tif"
-                weighted_service_group_area_poly_final = postprocws + "combined_service_group_by_value" + Suffix + ".shp"
-
-
+                weighted_service_group_area_ras = postprocws + "combined_service_group_by_area" + Suffix + ".tif"
+                weighted_service_group_area_poly_final = postprocws + "combined_service_group_by_area" + Suffix + ".shp"
 
                 gp.AddMessage("\nGrouping results by " + str(percent) + "%...")
                 num_slices = int(round(100 / int(percent)))
